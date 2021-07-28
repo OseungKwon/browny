a577372be0b1caf9ee8af865287bf711ef414450
korkt-kim <gnscott6@naver.com>
2021-07-28 09:58:47 +0900

    migrate minimal.cc

--- client/src/redux/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-import axios from "axios";
-import postSaga from './postSaga';
-
-export default function* rootSaga() {
-    yield all([fork(postSaga)]);
-}
